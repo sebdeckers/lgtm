e1ba132353ee3877adc47d2f4682265c841bce3f
Sebastiaan Deckers <seb@ninja.sg>
2015-10-02 12:27:50 +0800

    GitHub webhook handler for issue_comment

--- app/config.js
+++ app/config.js
@@ -3,3 +3,7 @@ const isDevelopment = !isProduction
 
 export const port = process.env.PORT || 3000
 export const logFormat = isDevelopment ? 'dev' : 'combined'
+export const github = {
+  path: '/github/webhook',
+  secret: process.env.GITHUB_WEBHOOK_SECRET || 'secret'
+}
--- app/index.js
+++ app/index.js
@@ -1,16 +1,25 @@
-import { port, logFormat } from './config'
+import * as config from './config'
 import http from 'http'
 import express from 'express'
 import morgan from 'morgan'
+import githubWebhookHandler from 'github-webhook-handler'
 
 const app = express()
+app.use(morgan(config.logFormat))
 
-app.use(morgan(logFormat))
+const handler = githubWebhookHandler(config.github)
+app.use(handler)
+handler.on('*', event => {
+  console.log(`webhook received: ${ event.event }`)
+})
+handler.on('issue_comment', ({ payload: { action, issue, comment } }) => {
+  if (action !== 'created') return
+  if (!issue.pull_request) return
+  console.log(`#${ issue.number } @${ comment.user.login }: ${ comment.body }`)
+})
 
-app.set('port', port)
-
+app.set('port', config.port)
 http
   .createServer(app)
   .listen(app.get('port'))
-
-console.log(`Listening on port ${ app.get('port') } ...`)
+console.log(`Listening on port ${ app.get('port') }`)
