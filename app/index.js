657982938eda41c3909fbef5099ce6123d6eb4e3
Sebastiaan Deckers <seb@ninja.sg>
2015-10-02 13:05:10 +0800

    Check for votes in message body

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import http from 'http'
 import express from 'express'
 import morgan from 'morgan'
 import githubWebhookHandler from 'github-webhook-handler'
+import escapeStringRegexp from 'escape-string-regexp'
 
 const app = express()
 app.use(morgan(config.logFormat))
@@ -12,9 +13,14 @@ app.use(handler)
 handler.on('*', event => {
   console.log(`webhook received: ${ event.event }`)
 })
+
+const votes = ['+1', ':+1:', '-1', ':-1:']
+  .map(pattern => new RegExp(escapeStringRegexp(pattern) + '(\\s|$)', 'm'))
+
 handler.on('issue_comment', ({ payload: { action, issue, comment } }) => {
   if (action !== 'created') return
   if (!issue.pull_request) return
+  if (!votes.some(vote => vote.test(comment.body))) return
   console.log(`#${ issue.number } @${ comment.user.login }: ${ comment.body }`)
 })
 
