8e52e1836d7c1bfacc80cff84cb536918bedf189
Sebastiaan Deckers <seb@ninja.sg>
2015-10-25 20:53:41 +0800

    Client side router fallback

--- src/backend/routes/assets/assets.js
+++ src/backend/routes/assets/assets.js
@@ -2,4 +2,8 @@ import express from 'express'
 import { nearestSync } from 'nearest-file-path'
 
 const directory = nearestSync('dist')
-export default express.static(directory)
+
+const router = new express.Router()
+router.use(express.static(directory))
+router.use((req, res, next) => res.sendFile('index.html', { root: directory }))
+export default router
