8298b18a5d5ce7d5e774c1588a59b41b1175a99b
Sebastiaan Deckers <seb@ninja.sg>
2015-10-26 19:40:15 +0800

    Use express-history-api-fallback middleware for client side routing with Express

--- src/backend/routes/assets/assets.js
+++ src/backend/routes/assets/assets.js
@@ -1,12 +1,9 @@
 import express from 'express'
+import fallback from 'express-history-api-fallback'
 import { nearestSync } from 'nearest-file-path'
 
 const router = new express.Router()
 const root = nearestSync('dist')
 router.use(express.static(root))
-router.get((req, res) => {
-  if (req.accepts('html')) {
-    res.sendFile('index.html', { root })
-  }
-})
+router.use(fallback('index.html', { root }))
 export default router
