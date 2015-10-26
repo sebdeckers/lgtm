7621874d88020ec51372d4fa10b3c5670af6c40b
Sebastiaan Deckers <seb@ninja.sg>
2015-10-26 12:59:45 +0800

    Added GET and HTML restrictions to static fallback

--- src/backend/routes/assets/assets.js
+++ src/backend/routes/assets/assets.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import { nearestSync } from 'nearest-file-path'
 
-const directory = nearestSync('dist')
-
 const router = new express.Router()
-router.use(express.static(directory))
-router.use((req, res, next) => res.sendFile('index.html', { root: directory }))
+const root = nearestSync('dist')
+router.use(express.static(root))
+router.get((req, res) => {
+  if (req.accepts('html')) {
+    res.sendFile('index.html', { root })
+  }
+})
 export default router
