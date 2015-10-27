c0fcf03cd865685cb532c9e31d5565bef776c71c
Sebastiaan Deckers <seb@ninja.sg>
2015-10-28 01:33:29 +0800

    Chain decorator for router methods

--- src/frontend/router.js
+++ src/frontend/router.js
@@ -1,21 +1,22 @@
 /* global location */
 import { on } from 'bubbly'
 import uriTemplates from 'uri-templates'
+import { chain } from 'js-deco/dist/commonjs/chain'
 
 export default class Router {
-  constructor (base = location.origin) {
-    this.base = base
-    this.routes = new Map()
+  constructor (routes = new Map()) {
+    this.routes = routes
     window::on('popstate', event => this.trigger())
   }
 
+  @chain
   route (pattern, handler) {
     const template = uriTemplates(pattern)
     const predicate = template.fromUri
     this.routes.set(predicate, handler)
-    return this
   }
 
+  @chain
   trigger () {
     const path = location.pathname + location.search + location.hash
     for (const [predicate, handler] of this.routes.entries()) {
