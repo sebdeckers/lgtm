98fa2d29bffa60a44b3dede1a4f797ac77adf974
Sebastiaan Deckers <seb@ninja.sg>
2015-10-30 11:08:03 +0800

    Intercept route handlers

--- src/frontend/choreographer.js
+++ src/frontend/choreographer.js
@@ -1,8 +1,11 @@
 import Router from './router'
 
 export default class extends Router {
-  constructor () {
-    super(...arguments)
-    console.log('places, everyone!')
+  constructor (routes = new Map(), ...args) {
+    for (const [pattern, handler] of new Map(routes).entries()) {
+      console.log(pattern, handler)
+      // TODO Wrap the handler with asynchronous transition management.
+    }
+    super(routes, ...args)
   }
 }
