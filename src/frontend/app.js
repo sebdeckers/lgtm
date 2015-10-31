1c646bc899c5357097b5f774d5e0b621d2105e74
Sebastiaan Deckers <seb@ninja.sg>
2015-10-31 14:11:45 +0800

    Instantiate custom element for route layout. Pass URL parameters as dataset.

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -6,17 +6,7 @@ import dom4 from 'dom4'
 import Choreographer from './choreographer'
 import loginElement from './components/Login'
 
-export const choreographer = new Choreographer([
-  ['/', () => {
-    try {
-      document.registerElement('lgtm-login', loginElement)
-    } catch (error) {}
-    const login = document.createElement('lgtm-login')
-    document.body.appendChild(login)
-  }],
-  ['/foo{?bar,test}', params => {
-    const message = document.createElement('pre')
-    message.textContent = `Hey it worked! \n ${JSON.stringify(params, null, 2)}`
-    document.body.appendChild(message)
-  }]
+export const choreographer = new Choreographer(document.body, [
+  ['/', { tagName: 'lgtm-login', element: loginElement }],
+  ['/foo{?bar,test}', { tagName: 'pre' }]
 ])
--- src/frontend/choreographer.js
+++ src/frontend/choreographer.js
@@ -1,11 +1,23 @@
 import Router from './router'
 
+function onRoute (params, { element, tagName }) {
+  try {
+    document.registerElement(tagName, element)
+  } catch (error) {}
+  const scene = document.createElement(tagName)
+  Object.assign(scene.dataset, params)
+  // TODO Add asynchronous transition management.
+  this.stage.appendChild(scene)
+}
+
 export default class extends Router {
-  constructor (routes = new Map(), ...args) {
-    for (const [pattern, handler] of new Map(routes).entries()) {
-      console.log(pattern, handler)
-      // TODO Wrap the handler with asynchronous transition management.
+  constructor (stage, scenes) {
+    const routes = new Map()
+    for (const [pattern, opts] of new Map(scenes).entries()) {
+      const handler = params => this::onRoute(params, opts)
+      routes.set(pattern, handler)
     }
-    super(routes, ...args)
+    super(routes)
+    this.stage = stage
   }
 }
